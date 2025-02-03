bddde87c2ffee3d45e804bf1a507009c1174d828
Алексей Ватютов <vatytovav@VatytovAV-18740.gbubdd.local>
2025-02-03 16:51:17 +0300

    first commut

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Переключаем экраны при клике на пункты меню
+    /*document.querySelectorAll('.nav-bar a').forEach(link => {
+        link.addEventListener('click', e => {
+            const target = document.getElementById(e.target.hash.slice(1));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });*/
+});
